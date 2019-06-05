ff98e7a45a43ab853d11abc9539f803a4cc3eee9
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-06-05 14:29:01 +0100

    Added two basic http GET methods to HelloWorld controller

--- MvcMovies/Controllers/HelloWorldController.cs
+++ MvcMovies/Controllers/HelloWorldController.cs
@@ -6,11 +6,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MvcMovies.Controllers
 {
+    // each method in here is an HTTP endpoint
     public class HelloWorldController : Controller
     {
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+            // GET /HelloWorld
+        public string Index()
         {
-            return View();
+            return "Hello world! this is my default action.";
+        }
+
+        // GET HelloWorld/Welcome
+        public string Welcome()
+        {
+            return "Welcome to the Films Database!";
         }
     }
 }
