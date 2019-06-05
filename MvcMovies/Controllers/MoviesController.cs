a478ac4939ce20586d47507e035d185e264ec979
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-06-05 16:49:07 +0100

    Added a movies controller so the link to Movies in navbar will go somewhere now

---
+++ MvcMovies/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovies.Models;
+using System.Diagnostics;
+
+
+namespace MvcMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
