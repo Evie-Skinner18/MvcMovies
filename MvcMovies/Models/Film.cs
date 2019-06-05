26499afa107b4b39df840593fc4579f898580468
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-06-05 17:45:45 +0100

    Started making the Film class in Models. Really understanding this on a deeper level now

---
+++ MvcMovies/Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovies.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public string Review { get; set; }
+        public decimal Price { get; set; }
+    }
+}
