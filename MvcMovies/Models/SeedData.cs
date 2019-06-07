aa2b8b707402f72c0b1772bc06b236c8399d9897
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-06-07 16:12:21 +0100

    Added a seed data class to seed the DB when the app starts up

--- MvcMovies/Models/SeedData.cs
+++ MvcMovies/Models/SeedData.cs
@@ -5,8 +5,60 @@ using System.Linq;
 
 namespace MvcMovies.Models
 {
-    public class SeedData
+    public static class SeedData
     {
+        public static void Initialise(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMoviesContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcMoviesContext>>()))
+            {
+                // context.Film is a Db Set which is like a list. This asks "are there any films in the Db set?"
+                if (context.Film.Any())
+                {
+                    return;   // if the .Any returns true, we don't need to do anything as DB has been seeded
+                }
 
+                // if there are no films, we need to put some films in the Db Set
+                context.Film.AddRange(
+                    new Film
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "",
+                        Review = "",
+                        Price = 7.99M
+                    },
+
+                    new Film
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "",
+                        Review = "",
+                        Price = 8.99M
+                    },
+
+                    new Film
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "",
+                        Review = "",
+                        Price = 9.99M
+                    },
+
+                    new Film
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "",
+                        Review = "",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+
+            }
+        }
     }
 }
--- MvcMovies/Program.cs
+++ MvcMovies/Program.cs
@@ -5,16 +5,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MvcMovies.Models;
 
 namespace MvcMovies
 {
     public class Program
-    {
+    {        
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            // try to seed the DB before running the app
+            using (var scope = host.Services.CreateScope())
+            {
+                // I think this is the dependency injection container
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MvcMoviesContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialise(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
