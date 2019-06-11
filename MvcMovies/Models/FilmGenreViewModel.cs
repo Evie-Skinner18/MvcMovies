180671852bca53a1f4d6c75ea9defd710c0400de
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-06-11 18:18:46 +0100

    Added a class for view to display genre to filter films by genre

---
+++ MvcMovies/Models/FilmGenreViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+
+namespace MvcMovies.Models
+{
+    public class FilmGenreViewModel
+    {
+        public List<Film> Films { get; set; }
+
+        // HTML select element so users can select an item
+        public SelectList Genres { get; set; }
+        public string FilmGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
